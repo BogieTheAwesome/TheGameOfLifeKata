787513229c61bfee9a78c66cb803571a5ebfd1b5
BogieTheAwesome <abogenri@gmail.com>
2013-11-17 23:08:09 -0600

    Cells Appropriately Calculate State

--- GameOfLifeKata/test/gameOfLife/GameOfLifeTest.java
+++ GameOfLifeKata/test/gameOfLife/GameOfLifeTest.java
@@ -18,4 +18,57 @@ public class GameOfLifeTest {
 		assertFalse(cell.IsAlive());
 	}
 
+	@Test
+	public void aLiveCellWithFewerThanTwoLiveNeighborsDiesFromStarvation() {
+		Cell cell = new Cell(true);
+		Cell topNeighbor = new Cell(true);
+		Cell bottomNeighbor = new Cell(false);
+		Cell rightNeighbor = new Cell(false);
+		Cell leftNeighbor = new Cell(false);
+		
+		cell.AddNeighbor(topNeighbor);
+		cell.AddNeighbor(bottomNeighbor);
+		cell.AddNeighbor(rightNeighbor);
+		cell.AddNeighbor(leftNeighbor);
+		
+		cell.calculateState();
+		
+		assertFalse(cell.IsAlive());
+	}
+	
+	@Test
+	public void aLiveCellWithMoreThanThreeLiveNeighborsDiesFromOverCrowding() {
+		Cell cell = new Cell(true);
+		Cell topNeighbor = new Cell(true);
+		Cell bottomNeighbor = new Cell(true);
+		Cell rightNeighbor = new Cell(true);
+		Cell leftNeighbor = new Cell(true);
+		
+		cell.AddNeighbor(topNeighbor);
+		cell.AddNeighbor(bottomNeighbor);
+		cell.AddNeighbor(rightNeighbor);
+		cell.AddNeighbor(leftNeighbor);
+		
+		cell.calculateState();
+		
+		assertFalse(cell.IsAlive());
+	}
+	
+	@Test
+	public void aDeadCellWithExactlyThreeLiveNeighborsIsRevived() {
+		Cell cell = new Cell(false);
+		Cell topNeighbor = new Cell(true);
+		Cell bottomNeighbor = new Cell(true);
+		Cell rightNeighbor = new Cell(true);
+		Cell leftNeighbor = new Cell(false);
+		
+		cell.AddNeighbor(topNeighbor);
+		cell.AddNeighbor(bottomNeighbor);
+		cell.AddNeighbor(rightNeighbor);
+		cell.AddNeighbor(leftNeighbor);
+		
+		cell.calculateState();
+		
+		assertTrue(cell.IsAlive());
+	}
 }
