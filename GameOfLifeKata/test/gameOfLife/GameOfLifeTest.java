4e2179a8d493be74de113151eca8c0bd8d629638
BogieTheAwesome <abogenri@gmail.com>
2013-11-18 20:12:57 -0600

    Refactor - Move Cell Tests to their own suite

--- GameOfLifeKata/test/gameOfLife/GameOfLifeTest.java
+++ GameOfLifeKata/test/gameOfLife/GameOfLifeTest.java
@@ -2,73 +2,13 @@ package gameOfLife;
 
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class GameOfLifeTest {
 
 	@Test
-	public void aLiveCellInformsUserThatItIsAlive() {
-		Cell cell = new Cell(true);
-		assertTrue(cell.IsAlive());
-	}
-	
-	@Test
-	public void aDeadCellInformsUserThatItIsNotAlive() {
-		Cell cell = new Cell(false);
-		assertFalse(cell.IsAlive());
-	}
-
-	@Test
-	public void aLiveCellWithFewerThanTwoLiveNeighborsDiesFromStarvation() {
-		Cell cell = new Cell(true);
-		Cell topNeighbor = new Cell(true);
-		Cell bottomNeighbor = new Cell(false);
-		Cell rightNeighbor = new Cell(false);
-		Cell leftNeighbor = new Cell(false);
-		
-		cell.AddNeighbor(topNeighbor);
-		cell.AddNeighbor(bottomNeighbor);
-		cell.AddNeighbor(rightNeighbor);
-		cell.AddNeighbor(leftNeighbor);
-		
-		cell.calculateState();
-		
-		assertFalse(cell.IsAlive());
-	}
-	
-	@Test
-	public void aLiveCellWithMoreThanThreeLiveNeighborsDiesFromOverCrowding() {
-		Cell cell = new Cell(true);
-		Cell topNeighbor = new Cell(true);
-		Cell bottomNeighbor = new Cell(true);
-		Cell rightNeighbor = new Cell(true);
-		Cell leftNeighbor = new Cell(true);
-		
-		cell.AddNeighbor(topNeighbor);
-		cell.AddNeighbor(bottomNeighbor);
-		cell.AddNeighbor(rightNeighbor);
-		cell.AddNeighbor(leftNeighbor);
-		
-		cell.calculateState();
-		
-		assertFalse(cell.IsAlive());
-	}
-	
-	@Test
-	public void aDeadCellWithExactlyThreeLiveNeighborsIsRevived() {
-		Cell cell = new Cell(false);
-		Cell topNeighbor = new Cell(true);
-		Cell bottomNeighbor = new Cell(true);
-		Cell rightNeighbor = new Cell(true);
-		Cell leftNeighbor = new Cell(false);
-		
-		cell.AddNeighbor(topNeighbor);
-		cell.AddNeighbor(bottomNeighbor);
-		cell.AddNeighbor(rightNeighbor);
-		cell.AddNeighbor(leftNeighbor);
-		
-		cell.calculateState();
-		
-		assertTrue(cell.IsAlive());
+	public void test() {
+		fail("Not yet implemented");
 	}
 }
